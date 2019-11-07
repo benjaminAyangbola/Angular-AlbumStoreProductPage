d89b703fb66d3ba4a5914d66514d3e6f99cd6386
Benjamin <benjaminayangbola@gmail.com>
2019-11-07 16:05:48 +0100

    Fix type assertion in product service

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,12 +14,12 @@ export class ProductService {
 
   getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl)
-      .map(response => <Album> response.json());
+      .map(response => <Album>response.json());
   }
 
   getProducts(): Observable<Product[]> {
     return this._http.get(this._productsUrl)
-      .map(response => <Product[]> response.json());
+      .map(response => <Product[]>response.json());
   }
 
 }
